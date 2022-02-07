70b0aa3dd4eb90d16d202c8475f8e33f8aab4fec
HYF <alinamarasca@gmail.com>
2022-02-07 10:06:18 +0100

    add redirect

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Intro from "./components/Intro";
 import GeneralInfo from "./components/GeneralInfo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       {/* <ModalProvider> */}
-      <div className="wrapper">
-        <Intro />
-        <GeneralInfo />
-        <About />
-        <Projects />
-      </div>
+      <Router>
+        <div className="wrapper">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Intro />
+                  <GeneralInfo />
+                  <About />
+                  <Projects />
+                </>
+              }
+            ></Route>
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
       {/* </ModalProvider> */}
     </>
   );
