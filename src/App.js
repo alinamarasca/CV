4bf4c0dc4a5f89e6c2bdc2457e8798fcac9df528
HYF <alinamarasca@gmail.com>
2022-02-07 15:29:42 +0100

    rafctored modal

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
-// import "./sass/App.scss";
-import React from "react";
-// import ModalProvider from "./contexts/ModalContext/ModalContextProvider";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Intro from "./components/Intro";
-import GeneralInfo from "./components/GeneralInfo";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate
 } from "react-router-dom";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Intro from "./components/Intro";
+import GeneralInfo from "./components/GeneralInfo";
+import Modal from "./components/shared/Modal";
+import ModalContext from "./contexts/ModalContext";
+import MessageForm from "./components/shared/MessageForm";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    console.log("close modal");
+    setIsOpen(false);
+  };
+  const openModal = () => {
+    console.log("open modal");
+    setIsOpen(true);
+  };
+
+  const modalControl = {
+    modal: isOpen,
+    closeModal,
+    openModal
+  };
+
   return (
     <>
-      {/* <ModalProvider> */}
-      <Router>
-        <div className="wrapper">
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <>
-                  <Intro />
-                  <GeneralInfo />
-                  <About />
-                  <Projects />
-                </>
-              }
-            ></Route>
-            <Route path="/*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
-      </Router>
-      {/* </ModalProvider> */}
+      <ModalContext.Provider value={modalControl}>
+        <Router>
+          <button onClick={e => openModal()}>CLICK</button>
+          <div className="wrapper">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <>
+                    <Intro />
+                    <GeneralInfo />
+                    <About />
+                    <Projects />
+                    <Modal open={isOpen} children={MessageForm()} />
+                  </>
+                }
+              ></Route>
+              <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </Router>
+      </ModalContext.Provider>
     </>
   );
 }
